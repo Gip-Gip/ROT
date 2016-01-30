325d5dd6d3fe6c5f9f7a003d60186397935d3652
Charles Thompson <cmt.16.yes@gmail.com>
2016-01-30 09:54:31 -0500

    Created rot_videoManager.c

---
+++ x86/rot_videoManager.c
@@ -0,0 +1,41 @@
+/* rot_videoManager.c - detects and manages video */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "rot_video.h"
+
+#define LOGID "rot_videoControl"
+
+#define MONOTERMFND "Monochrome terminal found!"
+#define COLRTERMFND "Colour terminal found!"
+
+extern void rot_terminalInit();
+extern void rot_terminalPrint();
+extern void rot_terminalClear();
+extern void rot_log();
+
+uint8_t *videoMemory = 0;
+bool terminal = false;
+
+void rot_videoInit( void )
+{
+    rot_terminalInit();
+    if(videoMemory == MONOTERMADDR)
+    {
+        terminal = true;
+        rot_terminalClear();
+        rot_log(LOGID, MONOTERMFND);
+    }
+
+    else if(videoMemory == COLRTERMADDR)
+    {
+        terminal = true;
+        rot_terminalClear();
+        rot_log(LOGID, COLRTERMFND);
+    }
+}
+
+void rot_print(char *_str_)
+{
+    if(terminal == true) rot_terminalPrint(_str_);
+}
