059e45d3fa35b552e8dd7f7d66cecd50fde485ae
Charles Thompson <cmt.16.yes@gmail.com>
2016-02-04 09:55:18 -0500

    Added terminal scrolling

--- x86/rot_terminal.c
+++ x86/rot_terminal.c
@@ -5,7 +5,7 @@
 #include "rot_video.h"
 
 extern uint8_t *videoMemory;
-unsigned int cursor = 0;
+unsigned int cursor = TERMSIZE - TERMWIDTH;
 
 void rot_terminalInit( void )
 {
@@ -14,6 +14,24 @@ void rot_terminalInit( void )
 
     else if((*(BDAVIDSTAT) & COLRTERM) == COLRTERM)
         videoMemory = COLRTERMADDR;
+}
+
+void rot_terminalScroll( void )
+{
+    cursor = TERMTRUEWIDTH + TERMDEPTH;
+    while(cursor != TERMSIZE)
+    {
+        *(videoMemory + cursor - TERMTRUEWIDTH) = *(videoMemory + cursor);
+        cursor ++;
+    }
+    cursor = TERMSIZE - TERMTRUEWIDTH;
+    while(cursor != TERMSIZE)
+    {
+        *(videoMemory + cursor) = 0;
+        *(videoMemory + cursor + 1) = TERMCOLOUR;
+        cursor += TERMDEPTH;
+    }
+    cursor = TERMSIZE - TERMTRUEWIDTH;
 }
 
 void rot_terminalPrint(uint8_t *__str)
@@ -32,6 +50,8 @@ void rot_terminalPrint(uint8_t *__str)
             cursor += TERMDEPTH;
             __str ++;
         }
+
+        if(cursor >= TERMSIZE) rot_terminalScroll();
     }
 }
 
@@ -49,5 +69,5 @@ void rot_terminalClear( void )
         cursor += TERMDEPTH;
     }
 
-    cursor = 0;
+    cursor = TERMSIZE - TERMTRUEWIDTH;
 }
